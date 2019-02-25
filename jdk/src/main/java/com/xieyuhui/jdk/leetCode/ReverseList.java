83056d9346d8740855e1880c9ef0eccc56d91ee4
xieyuhui <xieyuhu728@qq.comi>
2019-02-25 17:48:45 +0800

    add reverseList method

--- jdk/src/main/java/com/xieyuhui/jdk/leetCode/ReverseList.java
+++ jdk/src/main/java/com/xieyuhui/jdk/leetCode/ReverseList.java
@@ -3,7 +3,16 @@ package com.xieyuhui.jdk.leetCode;
 /**
  * @Auther: xieyuhui
  * @Date: 2019-02-25 16:39
- * @Description: 反转链表
+ * @Description: 反转链表：
+ * 保存当前头节点的下一个节点（当前头节点为2，先将节点3 保存起来）
+ * <p>
+ * 将当前头节点的下一个节点指向 “上一个节点”（当前头节点2 指向了 (“上一个节点”)节点1），这一步才是实现反转
+ * <p>
+ * 将当前头节点设置 “上一个节点”（将节点2 设为“上一个节点”）
+ * <p>
+ * 将保存的下一个节点设置 “头节点”（将节点3 设为“头节点”）
+ * <p>
+ * 链接：https://blog.csdn.net/xyh269/article/details/70238501
  */
 public class ReverseList {
 
@@ -37,29 +46,21 @@ public class ReverseList {
         }
     }
 
-    public static Node _reverseList(Node node) {
-        //当前节点
-        Node now = node;
-        //当前节点的前驱节点
-        Node prev = null;
-        //当前节点的后驱节点
+    public static Node _reverseList(Node head) {
+        Node pre = null;
         Node next = null;
-        //定义尾节点
-        Node tail = null;
-        while (now != null) {
-            //next 指向当前节点的下个节点
-            next = now.next;
-            //如果是尾节点
-            if (null == next) {
-                tail = now;
-            }
-            //链表指针反转，当前节点的next指针指向前一个节点。此时方式链表断裂
-            now.next = prev;
-            //前驱指针后移一个节点
-            prev = now;
-            //当前指针后移一个节点
-            now = next;
+        //循环至尾节点
+        while (head != null) {
+            //next一直保存当前节点的下个节点
+            next = head.next;
+            //反转，当前指向下个节点的指针指向上一个节点
+            head.next = pre;
+            //将当前头节点设置 “上一个节点”
+            pre = head;
+            //将保存的下一个节点设置为 “头节点”
+            head = next;
         }
-        return tail;
+        return pre;
+
     }
 }
