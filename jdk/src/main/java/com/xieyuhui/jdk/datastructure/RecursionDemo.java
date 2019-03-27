01c32faf4cd7118554863d58691e1782c5c336c6
xieyuhui <xieyuhui728@qq.com>
2019-03-27 21:30:43 +0800

    Subsets 子集

--- jdk/src/main/java/com/xieyuhui/jdk/datastructure/RecursionDemo.java
+++ jdk/src/main/java/com/xieyuhui/jdk/datastructure/RecursionDemo.java
@@ -4,18 +4,34 @@ package com.xieyuhui.jdk.datastructure;
  * @Auther: xieyuhui
  * @Date: 2019-03-27 19:35
  * @Description: 递归简论
+ * <p>
+ * 递归的四个基本法则：
+ * 1.基准情形：必须总要有某些基准的情形，它们不用递归就能求解。
+ * 2.不断推进：那些要递归求解的情形，递归调用必须总能朝着一个基准情形推进。
+ * 3.设计法则：假设所有的递归调用都能进行。
+ * 4.合成效益法则：在求解一个问题的同一个实例时，切勿在不同的递归调用中做重复性的工作。
  */
 public class RecursionDemo {
     public static void main(String[] args) {
-        int result = f(2);
+        int result = mod(411);
         System.out.println(result);
     }
 
     public static int f(int x) {
+        int result = 0;
         if (x == 0) {
             return 0;
         } else {
-            return 2 * f(x - 1) + x * x;
+            result = 2 * f(x - 1) + x * x;
         }
+        return result;
+    }
+
+
+    public static int mod(int n) {
+//        return n % 100;
+        //取模公式 A Mod B=A-(A div B) * B （div含义为整除）
+        return n - (n / 100) * 100;
+
     }
 }
--- jdk/src/main/java/com/xieyuhui/jdk/leetCode/SubSets.java
+++ jdk/src/main/java/com/xieyuhui/jdk/leetCode/SubSets.java
@@ -1,5 +1,8 @@
 package com.xieyuhui.jdk.leetCode;
 
+import com.alibaba.fastjson.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,10 +15,29 @@ import java.util.List;
 public class SubSets {
 
     public static void main(String[] args) {
-
+        int[] nums = {1, 2};
+        List<List<Integer>> result = solution(nums);
+        System.out.println(JSONObject.toJSONString(result));
     }
 
     public static List<List<Integer>> solution(int[] nums) {
-        return null;
+        List<List<Integer>> result = new ArrayList<>();
+        result.add(new ArrayList<>());
+        if (nums == null || nums.length == 0) {
+            return result;
+        }
+        //思路:
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> res2 = new ArrayList<>(result);
+            for (int j = 0; j < res2.size(); j++) {
+                List<Integer> list = res2.get(j);
+                List<Integer> tmp = new ArrayList<>(list);
+                tmp.add(nums[i]);
+                result.add(tmp);
+            }
+        }
+        return result;
     }
+
+
 }
